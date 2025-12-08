d090db9a20725500861a447450b33a11dea2c43d
Tanvir Ahmed <tanver018765@gmail.com>
2025-12-08 21:12:12 +0600

    update

--- src/http/client.ts
+++ src/http/client.ts
@@ -1,10 +1,41 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_API_URL,
-    withCredentials: true, // eta na dile amader cookie jeta server theke ashbe seta browser e save hobe na and request er sathe cookie server e jabe na 
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json", // client json data accept korbe
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  withCredentials: true, // eta na dile amader cookie jeta server theke ashbe seta browser e save hobe na and request er sathe cookie server e jabe na
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json", // client json data accept korbe
+  },
+});
+
+const refreshToken = async () => {
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+    {},
+    {
+      withCredentials: true,
     }
-})
+  );
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401) {
+      try {
+        const headers = { ...originalRequest.headers };
+        await refreshToken();
+        return api.request({ ...originalRequest, headers });
+      } catch (error) {
+        console.warn("Error while getting Refresh our Access Token!");
+        useAuthStore.getState().logout();
+        return Promise.reject(error);
+      }
+      return Promise.reject(error);
+    }
+  }
+);
